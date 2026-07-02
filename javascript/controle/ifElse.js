d757f198647e31d667cd3c1b0460363d05b7e166
Guilherme Silva <guilrs.dev@gmail.com>
2026-07-02 16:49:10 -0300

    Estruturas de controle

---
+++ javascript/controle/ifElse.js
@@ -0,0 +1,10 @@
+const imprimirResutltado = function(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado');
+    } else {
+        console.log('Reprovado');
+    }
+}
+
+imprimirResutltado(10);
+imprimirResutltado(4);
