dee719b3d41ab46449a7715ba95f291c88d417c5
Guilherme Silva <guilrs.dev@gmail.com>
2026-07-06 22:56:31 -0300

    Funções

---
+++ javascript/funcao/factory1.js
@@ -0,0 +1,18 @@
+// const prod1 = {
+//     nome: '...',
+//     preco: 45
+// }
+
+// const prod1 = {
+//     nome: '...',
+//     preco: 1234
+// }
+
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa());
